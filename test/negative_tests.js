40a9e69bb1667a700db24888a176b5b30ae33d67
Charlie Kong <charlie@undercurrent.tech>
2022-05-13 16:06:52 +0800

    negative test skeleton code

---
+++ test/negative_tests.js
@@ -0,0 +1,7 @@
+const { Runtime, AccountStore } = require("@algo-builder/runtime");
+const { assert } = require("chai");
+const algosdk = require("algosdk");
+
+describe("Stateful Smart Contract Negative Tests", function () {
+    // write your code here
+});
