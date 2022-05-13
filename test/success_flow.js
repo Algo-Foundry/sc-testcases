a5d63a774f7569b6ef84acda689fe60b36a2af79
Charlie Kong <charlie@undercurrent.tech>
2022-05-13 16:05:50 +0800

    success flow skeleton

---
+++ test/success_flow.js
@@ -0,0 +1,9 @@
+const { convert } = require("@algo-builder/algob");
+const { Runtime, AccountStore } = require("@algo-builder/runtime");
+const { types } = require("@algo-builder/web");
+const { assert } = require("chai");
+const algosdk = require("algosdk");
+
+describe("Stateful Smart Contract Positive Tests", function () {
+    // write your code here
+});
