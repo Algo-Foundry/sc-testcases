d3ebc99bf6ed242de134b0fa612153bd2fbaf0bb
Charlie Kong <charlie@undercurrent.tech>
2023-01-13 18:28:57 +0800

    package updates

--- scripts/actions/reward.js
+++ scripts/actions/reward.js
@@ -1,37 +1,41 @@
-const { executeTransaction, convert, readAppGlobalState } = require("@algo-builder/algob");
+const { convert, readAppGlobalState } = require("@algo-builder/algob");
 const { types } = require("@algo-builder/web");
 const algosdk = require("algosdk");
 
 async function run(runtimeEnv, deployer) {
     const master = deployer.accountsByName.get("master");
-    const approvalFile = "game_approval.py";
-    const clearStateFile = "game_clearstate.py";
+    const appName = "gameApp";
 
     // get app info
-    let gameApp = deployer.getApp(approvalFile, clearStateFile);
+    let gameApp = deployer.getApp(appName);
     const appID = gameApp.appID;
     const gameAppAddress = gameApp.applicationAccount;
     let globalState = await readAppGlobalState(deployer, master.addr, appID);
     console.log(globalState);
+
+    // amount before
+    let appAccount = await deployer.algodClient.accountInformation(gameAppAddress).do();
+    const amountBefore = appAccount["amount"];
 
     // get mvp address
     const mvp = algosdk.encodeAddress(Buffer.from(globalState.get("Mvp"), 'base64'));
 
     // reward
     const rewardAppArgs = ["Reward"].map(convert.stringToBytes);
-    await executeTransaction(deployer, {
+    await deployer.executeTx({
         type: types.TransactionType.CallApp,
         sign: types.SignType.SecretKey,
         fromAccount: master,
         appID: appID,
-        payFlags: { totalFee: 1000 },
+        payFlags: { totalFee: 2000 }, // creator will pay for the inner txn fees
         appArgs: rewardAppArgs,
         accounts: [mvp]
     });
 
     // get app account balance
-    let appAccount = await deployer.algodClient.accountInformation(gameAppAddress).do();
-    console.log(appAccount);
+    appAccount = await deployer.algodClient.accountInformation(gameAppAddress).do();
+    console.log("contract amount before: ", amountBefore);
+    console.log("contract amount after: ", appAccount["amount"]);
 }
 
 module.exports = { default: run };
--- test/common/commonfn.js
+++ test/common/commonfn.js
@@ -1,64 +1,67 @@
 const { convert } = require("@algo-builder/algob");
 const { types } = require("@algo-builder/web");
 
-const initGameContract = (runtime, creatorAccount, health, approvalFile, clearStateFile) => {
+const initGameContract = (runtime, creatorAccount, health, approvalProgramFilename, clearProgramFilename) => {
+    const appName = "gameApp";
+    
     // deploy contract
     runtime.deployApp(
-        approvalFile,
-        clearStateFile,
+        creatorAccount,
         {
-            sender: creatorAccount,
+            approvalProgramFilename,
+            clearProgramFilename,
+            metaType: types.MetaType.FILE,
+            appName,
             localInts: 1,
             localBytes: 0,
             globalInts: 2,
             globalBytes: 1,
-            appArgs: [convert.uint64ToBigEndian(health)],
+            appArgs: [convert.uint64ToBigEndian(health)]
         },
         { totalFee: 1000 }, //pay flags
-        {} //smart contract template params
     );
 
-    const appInfo = runtime.getAppInfoFromName(approvalFile, clearStateFile);
+    const appInfo = runtime.getAppByName(appName);
     const appAddress = appInfo.applicationAccount;
 
     // fund the contract
-    runtime.executeTx({
+    runtime.executeTx([{
         type: types.TransactionType.TransferAlgo,
         sign: types.SignType.SecretKey,
         fromAccount: creatorAccount, //use the account object
         toAccountAddr: appAddress, //app address
         amountMicroAlgos: 2e7, //20 algos
-        payFlags: { totalFee: 1000 },
-    });
+        payFlags: { totalFee: 1000 }
+    }]);
 
     return appInfo;
 };
 
 const optIn = (runtime, account, appID) => {
-    runtime.executeTx({
+    runtime.executeTx([{
         type: types.TransactionType.OptInToApp,
         sign: types.SignType.SecretKey,
         fromAccount: account,
         appID: appID,
         payFlags: { totalFee: 1000 },
-    });
+    }]);
 };
 
 const attack = (runtime, account, appID) => {
     const attackAppArgs = ["Attack"].map(convert.stringToBytes);
-    runtime.executeTx({
+    runtime.executeTx([{
         type: types.TransactionType.CallApp,
         sign: types.SignType.SecretKey,
         fromAccount: account,
         appID: appID,
         payFlags: { totalFee: 1000 },
         appArgs: attackAppArgs,
-    });
+    }]);
 };
 
 const rewardPlayer = (runtime, sender, mvp_address, appID) => {
     const rewardAppArgs = ["Reward"].map(convert.stringToBytes);
-    return runtime.executeTx({
+    return runtime.executeTx([{
         type: types.TransactionType.CallApp,
         sign: types.SignType.SecretKey,
         fromAccount: sender,
@@ -66,7 +69,7 @@ const rewardPlayer = (runtime, sender, mvp_address, appID) => {
         payFlags: { totalFee: 1000 },
         appArgs: rewardAppArgs,
         accounts: [mvp_address],
-    });
+    }]);
 };
 
 module.exports = {
